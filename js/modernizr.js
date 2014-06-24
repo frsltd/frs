9a4f2add6837a2cd7d83b059ba149327c416a61e
Christopher Nicholson <chris.nicholson@frsltd.com>
2014-06-24 13:23:49 +0100

    Adding changes from MO

--- js/modernizr.js
+++ js/modernizr.js
@@ -622,7 +622,7 @@ window.Modernizr = (function( window, document, undefined ) {
          * webkit.org/blog/175/introducing-css-gradients/
          * developer.mozilla.org/en/CSS/-moz-linear-gradient
          * developer.mozilla.org/en/CSS/-moz-radial-gradient
-         * dev.w3.org/csswg/css3-img/#gradients-
+         * dev.w3.org/csswg/css3-images/#gradients-
          */
 
         var str1 = 'background-image:',
