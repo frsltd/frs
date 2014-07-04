368a93b22f15fc63c3bc0ec7994bda97e4183f1f
Christopher Nicholson <chris.nicholson@frsltd.com>
2014-07-04 11:44:08 +0100

    Adding the contact form

---
+++ js/contact-validation.js
@@ -0,0 +1,33 @@
+function checkForm() {
+	name = document.getElementById("name").value;
+	email = document.getElementById("email").value;
+	hideAllErrors()
+	if (name == "")
+	{
+		document.getElementById("nameError").style.display = "inline";
+		return false;
+	}
+	else if (email == "")
+	{
+		document.getElementById("emailError").style.display = "inline";
+		return false;
+	}
+
+	if (!checkEmail(email))
+	{
+		document.getElementById("emailInvalid").style.display = "inline";
+		return false;
+	}
+	return true;
+}
+
+function hideAllErrors() {
+	document.getElementById("nameError").style.display = "none";
+	document.getElementById("emailError").style.display = "none";
+	document.getElementById("emailInvalid").style.display = "none";
+}
+
+function checkEmail(inputvalue) {
+	var pattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	return pattern.test(inputvalue);
+} 
